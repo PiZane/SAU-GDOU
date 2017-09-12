f353eba4ea848e55b78329f483a8a2373cfae9e1
pizane <pi@echo.cool>
2017-09-12 12:59:09 +0800

    fix main 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 window.onload = function() {
 	$.material.init();
+	var postHeight = $(".entry-content>iframe").contents().find('.post-iframe').height();
+	if (postHeight) {
+		$(".entry-content").height(postHeight);
+	}
 	$('.slider').unslider({
 		autoplay: true
 	});
