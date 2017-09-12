0a26f99e3e2764281de78bc3505a8b455146dd0a
pizane <pi@echo.cool>
2017-09-12 18:20:11 +0800

    fix main 6

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,18 @@ $(document).ready(function() {
 	$.material.init();
 	function content() {
 		var iframe = $(".entry-content>iframe").contents();
-		var postHeight = iframe.find('.post-iframe').height();
-		if (postHeight) {
-			$(".entry-content").height(postHeight);
-			$(".entry-content>iframe").height(postHeight);
-		}
-		var backgrounds = iframe.find('[style*="http://mmbiz.qpic.cn"]');
-		backgrounds.each(function(){
-			var url = $(this).css('background-image').slice(12, -1).replace(/"/g, "");;
-			url = 'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=' + url;
-			$(this).css('background-image', 'url("'+url+'")')
+		iframe.on("load", function() {
+			var postHeight = iframe.find('.post-iframe').height();
+			if (postHeight) {
+				$(".entry-content").height(postHeight);
+				$(".entry-content>iframe").height(postHeight);
+			}
+			var backgrounds = iframe.find('[style*="http://mmbiz.qpic.cn"]');
+			backgrounds.each(function(){
+				var url = $(this).css('background-image').slice(12, -1).replace(/"/g, "");;
+				url = 'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl=' + url;
+				$(this).css('background-image', 'url("'+url+'")')
+			});
 		});
 	}
 	content();
