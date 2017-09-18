a166b0b75bbd4f353ede336c7c4c4cb6f0931c2c
pizane <pi@echo.cool>
2017-09-18 09:51:31 +0800

    2

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="site-info" style="color:#fff;">
-						<p><a href="https://mp.weixin.qq.com/mp/homepage?__biz=MjM5NDgxMjM5Ng==&hid=5&sn=8d7a7cd5285659085acd7694b46af2ac&scene=18&pass_ticket=bViwsGkLjY7BNG12Muar6G8%2B25g2jhEiiieZteOErlb27LyJXb%2BO6Q8LKAi2zWf4">官方微信</a> | <a href="#">官方微博</a> | <a href="<?php echo get_page_link(get_page_by_path('my-page-slug')->ID); ?>">联系我们</a></p>
+						<p><a href="https://mp.weixin.qq.com/mp/homepage?__biz=MjM5NDgxMjM5Ng==&hid=5&sn=8d7a7cd5285659085acd7694b46af2ac&scene=18&pass_ticket=bViwsGkLjY7BNG12Muar6G8%2B25g2jhEiiieZteOErlb27LyJXb%2BO6Q8LKAi2zWf4">官方微信</a> | <a href="http://weibo.com/gdousl20100605">官方微博</a> | <a href="<?php echo get_page_link(get_page_by_path('contact-us')->ID); ?>">联系我们</a></p>
 						<!-- <span>电话: 010-00000000</span><br/>
 						<span>邮编: 524000</span><br/>
 						<span>地址: 广东省湛江市麻章区广东海洋大学体育楼</span> -->
